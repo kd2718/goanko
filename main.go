73b1a8c3ceb72e426668c207d09d7f06e079e328
kory donati <koryd@bixly.com>
2016-12-16 23:40:43 -0800

    added anko to main.go

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
   "fmt"
+  "github.com/fernandosanchezjr/anko/vm"
 )
 
+var env = vm.NewEnv()
+
+
 func main() {
-  fmt.printLn("hello")
+  env.Define("blast", func(name string) {
+    println(name, "You just got BLASTED!!!")
+  })
+  fmt.Println("hello")
+  who := "Kory Donati"
+  exe_str := fmt.Sprint("blast(\"", who, "\")")
+  env.Execute(exe_str)
+
 }
